c1bb02dce4849dc74350db07b0b2e630cb2e0f52
GDLAB-MSI02\GD Student <wuyiji199632@gmail.com>
2022-04-25 10:32:55 +0800

    upload

---
+++ FruitNinja Clone - Copy/Assets/Scripts/GameManager.cs
@@ -0,0 +1,90 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    // We need to counter the score for game manager
+    public int score;
+    public int HighestScore;
+    public Text ScoreText;
+    public Text HighScoreText;
+    public Text TimerText;
+    public GameObject GameOverPanel;
+    public AudioClip[] SliceSounds;
+    private AudioSource AudioSource;
+    public bool IsSlicingFruit = false;
+    public float TimeDuringNotSlicingFruits;
+    public float TimeDuringResetting;
+    public bool Reset = false;
+    void Start()
+    {
+        GameOverPanel.SetActive(false);
+        HighestScore = PlayerPrefs.GetInt("HighestScore");
+        HighScoreText.text = "Highest Score: " + HighestScore;
+        AudioSource = GetComponent<AudioSource>();
+        TimerText.text = "Time Before Fail: "+ TimeDuringNotSlicingFruits.ToString();
+    }
+     void Update()
+     {
+        CheckSlicingFruits();
+     }
+    public void IncreaseScore(int points)
+    {
+        score += points;
+        ScoreText.text = score.ToString();
+        if(score>=HighestScore)
+        {
+            PlayerPrefs.SetInt("HighestScore", score);
+            HighScoreText.text = score.ToString();
+        }
+        
+        
+    }
+    public void OnBombHit()
+    {
+        Time.timeScale = 0;
+        Debug.Log("I hit the bomb!");
+        GameOverPanel.SetActive(true);
+    }
+    public void PlayRandomSound()
+    {
+        AudioClip Sounds = SliceSounds[Random.Range(0, SliceSounds.Length)];
+        AudioSource.PlayOneShot(Sounds);
+    }
+    public void CheckSlicingFruits()
+    {
+        if (!IsSlicingFruit)
+        {
+            TimeDuringNotSlicingFruits -= Time.deltaTime;
+            TimerText.text = "Time Before Fail: " + TimeDuringNotSlicingFruits.ToString();
+            if (TimeDuringNotSlicingFruits <= 0)
+            {
+                OnBombHit();
+            }
+        }
+        else
+        {
+            TimeDuringNotSlicingFruits = 20f;
+            IsSlicingFruit = true;
+            if (IsSlicingFruit)
+            {
+                TimeDuringResetting -= Time.deltaTime;
+                if (TimeDuringResetting <= 0)
+                {
+                    TimeDuringResetting = 0;
+                    IsSlicingFruit = false;
+                    Reset = true;
+                }
+                if (Reset)
+                {
+                    TimeDuringResetting = 1f;
+                    TimeDuringResetting -= Time.deltaTime;
+                    Reset = false;
+                }
+            }
+        }
+
+    }
+}
